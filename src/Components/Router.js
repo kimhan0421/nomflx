d5a5a2b6477dc57b9ae4a319e04931779a930907
kimhan0421 <kimhan0421@naver.com>
2020-07-26 19:05:07 +0900

    2.2 React Router part Two

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,16 +1,17 @@
 import React, { HashRouter } from "react";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route, Redirect, Switch} from "react-router-dom";
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
 
 export default () => (
     <Router>
-      <>
-        <Route path="/" component={Home} />
-        <Route path="/tv"component={TV} />
-        <Route path="/tv/pop" render={() => <h1>Pop</h1>} />
-        <Route path="/search" component={Search} />
-      </>
+        <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/tv" exact component={TV} />
+            <Route path="/tv/pop" render={() => <h1>Pop</h1>} />
+            <Route path="/search" component={Search} />
+            <Redirect from="*" to="/" />
+        </Switch>
     </Router>
-  );
+);
