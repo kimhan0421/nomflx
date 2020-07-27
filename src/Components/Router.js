8f22e83154d280a99f0f1569a28adc5176155bdb
kimhan0421 <kimhan0421@naver.com>
2020-07-27 22:14:43 +0900

    3.2 CSS in React part Three

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -2,16 +2,20 @@ import React, { HashRouter } from "react";
 import {BrowserRouter as Router, Route, Redirect, Switch} from "react-router-dom";
 import Home from "Routes/Home";
 import TV from "Routes/TV";
+import Header from "Components/Header";
 import Search from "Routes/Search";
 
 export default () => (
     <Router>
-        <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/tv" exact component={TV} />
-            <Route path="/tv/pop" render={() => <h1>Pop</h1>} />
-            <Route path="/search" component={Search} />
-            <Redirect from="*" to="/" />
-        </Switch>
+        <>
+            <Header />
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/tv" exact component={TV} />
+                <Route path="/tv/pop" render={() => <h1>Pop</h1>} />
+                <Route path="/search" component={Search} />
+                <Redirect from="*" to="/" />
+            </Switch>
+        </>
     </Router>
 );
