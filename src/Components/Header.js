186b7053f2ebc4f90d0769ffa0a5f142126b7d4f
kimhan0421 <kimhan0421@naver.com>
2020-07-28 12:53:52 +0900

    3.4 Location Aware Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -21,9 +21,12 @@ const List = styled.ul`
 `;
 
 const Item = styled.li`
-  width: 80px;
-  height: 50px;
-  text-align: center;
+    width: 80px;
+    height: 50px;
+    text-align: center;
+    border-bottom: 5px solid 
+        ${props => (props.current ? "#3498db": "transparent")};
+    transition: border-bottom .3s ease-in-out;
 `;
 
 const SLink = styled(Link)`
