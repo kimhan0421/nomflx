b07860a354b5c7168599eb5582d1ffdf46e4fc01
kimhan0421 <kimhan0421@naver.com>
2020-08-03 03:00:49 +0900

    5.4 Search Container

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SearchPresenter from "./SearchPresenter";
+import { moviesApi } from "../../api";
 
 export default class extends React.Component{
     state = {
@@ -10,6 +11,27 @@ export default class extends React.Component{
         loading: false
     };
 
+    handleSubmit = () => {
+        const { searchTerm} = this.state;
+        if(searchTerm !== ""){
+            this.searchByTerm();
+        }
+    }
+
+    searchByTerm = (term) => {
+        const { searchTerm} = this.state;
+        this.setState({loading: true});
+        try{
+            const {data: {results: movieResults}}  = await moviesApi.search(searchTerm);
+            const {data: {results: showResults}} = await tvApi.search(searchTerm);
+            this.setState({movieResults, tvResult});            
+        }catch{
+            this.setState({error: " Can't find results."});
+        }finally{
+            this.setState({loading: false});
+        }
+    }
+
     render(){
         const {movieResults, tvResults, searchTerm, error, loading} = this.state;
         return <SearchPresenter
@@ -18,6 +40,7 @@ export default class extends React.Component{
             searchTerm={searchTerm}
             error={error}
             loading={loading}
+            handleSubmit={this.handleSubmit}
         />;
     }
 }
