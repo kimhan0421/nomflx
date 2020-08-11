6e170dd45c69f651b8cefa7251ce4755a8b74ba3
kimhan0421 <kimhan0421@naver.com>
2020-08-11 18:30:48 +0900

    6.3

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -2,7 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
-const SearchPresenter = (  movieResults, tvResults, searchTerm, handleSubmit, loading, error,) => null;
+const Container = styled.div``;
+
+const Form = styled.form``;
+
+const Input = styled.input``;
+
+const SearchPresenter = ( {
+    movieResults, tvResults, searchTerm, handleSubmit, loading, error
+} ) => <Container>
+    <Form onSubmit={handleSubmit}>
+
+    </Form>
+</Container>;
 
 SearchPresenter.propTypes = {
     movieResults: PropTypes.array,
